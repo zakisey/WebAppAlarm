96b1fcc6fb3cbc2890dfc19becb0f1a410c4c5c7
zakisey <yasuhiiro.sezaki@diva.co.jp>
2017-07-19 17:53:26 +0900

    added timer feature

---
+++ WebAppAlarm/WebAppAlarm/wwwroot/js/clock.js
@@ -0,0 +1,27 @@
+﻿function clock() {
+    var weeks = new Array("Sun", "Mon", "Thu", "Wed", "Thr", "Fri", "Sat");
+    var now = new Date();
+    var y = now.getFullYear();
+    var mo = now.getMonth() + 1;
+    var d = now.getDate();
+    var w = weeks[now.getDay()];
+    var h = now.getHours();
+    var mi = now.getMinutes();
+    var s = now.getSeconds();
+
+    // 日付時刻文字列のなかで常に2ケタにしておきたい部分はここで処理
+    if (mo < 10) mo = "0" + mo;
+    if (d < 10) d = "0" + d;
+    if (mi < 10) mi = "0" + mi;
+    if (s < 10) s = "0" + s;
+
+    //　HTML: <span id="clock_date">(ココの日付文字列を書き換え)</span>
+    document.getElementById("clock_date").innerHTML = y + "/" + mo + "/" + d + " (" + w + ")";
+    //　HTML: <span id="clock_time">(ココの時刻文字列を書き換え)</span>
+    document.getElementById("clock_time").innerHTML = h + ":" + mi + ":" + s;
+    //　HTML: <div id="clock_frame">の内部要素のフォントサイズをウインドウサイズの10分の1ピクセルに設定
+    // document.getElementById("clock_frame").style.fontSize = window.innerWidth / 10 + "px";
+}
+
+// 上記のclock関数を1000ミリ秒ごと(毎秒)に実行する
+setInterval(clock, 1000);
---
+++ WebAppAlarm/WebAppAlarm/wwwroot/js/player.js
@@ -0,0 +1,68 @@
+﻿var player;
+var timerHour;
+var timerMinute;
+var timerSecond;
+var timer;
+
+var userVideoId;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: userVideoId,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+function timerFunc() {
+    var now = new Date();
+    var h = now.getHours();
+    var m = now.getMinutes();
+    var s = now.getSeconds();
+
+    if (h == timerHour && m == timerMinute && s == timerSecond) {
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        clearInterval(timer);
+    }
+}
+
+function getIdFromUrl(url) {
+    var pos = url.search("v=");
+    var id = url.slice(pos + 2, pos + 13);
+    console.log(id);
+
+    return id;
+}
+
+$(document).ready(function() {
+    $("#buttonSet").click(function () {
+        timerHour = $("#inputHour").val();
+        timerMinute = $("#inputMinute").val();
+        timerSecond = $("#inputSecond").val();
+        timer = setInterval(timerFunc, 1000);
+        userVideoId = getIdFromUrl($("#inputUrl").val());
+    });
+});
